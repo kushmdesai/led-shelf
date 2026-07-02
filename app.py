955ad3c3a0cda0f17854f1c8de06d4b2ed9dbb6c
kush <kushdesai221@gmail.com>
2026-07-02 10:18:21 -0400

    mqtt implementation

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, jsonify
+from mqtt import send_message
 
 app = Flask(__name__)
 
@@ -13,6 +14,12 @@ def control():
 @app.route("/settings")
 def settings():
     return render_template('settings.html')
+
+@app.route("/api/power", methods = ['POST'])
+def power():
+    response = request.get_json()
+    send_message("power", response.get('state'))
+    return {'status': 'finished'}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
--- mqtt.py
+++ mqtt.py
@@ -1,6 +1,8 @@
 import paho.mqtt.client as mqtt
 from paho.mqtt.enums import CallbackAPIVersion
 
-mqttc = mqtt.Client(CallbackAPIVersion.VERSION2)
-mqttc.connect("localhost", 1883, 60)
-mqttc.publish("ledshelf/power", "ON")
+
+def send_message(channel_id, message):
+    mqttc = mqtt.Client(CallbackAPIVersion.VERSION2)
+    mqttc.connect("localhost", 1883, 60)
+    mqttc.publish(f"ledshelf/{channel_id}", message)
